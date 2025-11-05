e348d2dc1964ead7a5eaeb0950e2f906c64a5df9
t5p00n <tsoonping@gmail.com>
2025-11-05 23:11:05 +0800

    feat/finish login method for user class. make use of mvc ish? still trying to understand mvc

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,20 +1,56 @@
 package src.controller;
+
 import src.DataStore;
 import src.entity.User;
-
 
 public class UserController {
     private User currentUser;
     private DataStore dataStore;
 
-    public UserController(){
+    public UserController() {
         this.dataStore = new DataStore();
     }
 
-    public void login(){}//if can login, currentUser = user, else null
-    public void logout(){currentUser = null;}
-    public User getCurrentUser(){
+    public void setCurrentUser(User u) {
+        this.currentUser = u;
+    }
+
+    public User getCurrentUser() {
         return currentUser;
     }
-    public boolean updatePassword(){}
+
+    public boolean login(String userName, String pw) {
+        // check the userName and pw against dataStore
+        for (User u : dataStore.getStudentList()) {
+            if (u.getName() == userName && u.getPassword() == pw) {
+                setCurrentUser(u);
+                return true;
+            }
+        }
+        for (User u : dataStore.getCareerCenterStaffList()) {
+            if (u.getName() == userName && u.getPassword() == pw) {
+                setCurrentUser(u);
+                return true;
+            }
+        }
+        for (User u : dataStore.getCompanyRepresentativeList()) {
+            if (u.getName().equals(userName) && u.getPassword().equals(pw)) {
+                setCurrentUser(u);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void logout() {
+        setCurrentUser(null);
+    }
+
+    public boolean updatePassword(String oldPW, String newPW) {
+        if (getCurrentUser().getPassword().equals(oldPW)) {
+            getCurrentUser().setUserId(newPW);
+            return true;
+        }
+        return false;
+    }
 }
--- src/view/UserView.java
+++ src/view/UserView.java
@@ -1,5 +1,5 @@
 package src.view;
-// import src.entity.User;
+
 import src.controller.UserController;
 import java.util.Scanner;
 
@@ -7,30 +7,49 @@ public class UserView {
     private UserController userController;
     private Scanner sc = new Scanner(System.in);
 
-    public UserView(UserController userController){
-        this.userController = userController;
+    public UserView(UserController userController) {
+        setUserController(userController);
     }
 
-    public String[] displayLogin(){
+    public UserController getUserController() {
+        return this.userController;
+    }
+
+    public void setUserController(UserController uc) {
+        this.userController = uc;
+    }
+
+    public void login() {
+        // Show Login and ask for login credentials
         System.out.println("Login");
         System.out.print("Enter UserId: ");
         String userId = sc.nextLine();
         System.out.print("Enter Password: ");
         String password = sc.nextLine();
-        return new String[] {userId, password};
+
+        boolean loginSuccess = getUserController().login(userId, password);
+        if (loginSuccess) {
+            displayMessage("Login Successful!");
+        } else {
+            displayMessage("Login Failed! Please check your UserId and Password.");
+        }
+
     }
 
-    public int displayUserMenu(){return 0;}
-    public String[] displayChangePassword(){
+    public int displayUserMenu() {
+        return 0;
+    }
+
+    public String[] displayChangePassword() {
         System.out.println("Change Password");
         System.out.print("Enter Old Password: ");
         String oldPW = sc.nextLine();
         System.out.print("Enter New Password: ");
         String newPW = sc.nextLine();
-        return new String[]{oldPW, newPW};
+        return new String[] { oldPW, newPW };
     }
 
-    public void displayMessage(String msg){
+    public void displayMessage(String msg) {
         System.out.println(msg);
     }
 
