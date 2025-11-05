8a47397eae0a848e7b34d0802839e17cddd11fcf
t5p00n <tsoonping@gmail.com>
2025-11-05 23:11:26 +0800

    feat/main program. test user login

---
+++ src/ProjectApp.java
@@ -0,0 +1,18 @@
+package src;
+
+// import src.entity.*;
+import src.controller.*;
+import src.view.*;
+// import src.*;
+
+public class ProjectApp {
+    public static void main(String args[]) {
+        System.out.println("This is the main program");
+
+        // testing user login
+        UserController uc = new UserController();
+        UserView uw = new UserView(uc);
+        uw.login();
+
+    }
+}
