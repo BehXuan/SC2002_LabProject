ed7de166e00f27a05a06fe20a6de833ef11c3e86
Beh Wei Xuan <behweixuan@gmail.com>
2025-11-05 23:46:01 +0800

    Created career center staff controller

--- src/entity/CareerCenterStaff.java
+++ src/entity/CareerCenterStaff.java
@@ -1,5 +1,24 @@
 package src.entity;
 
 public class CareerCenterStaff extends User{
+    
+    private String staffDepartment;
+
     public CareerCenterStaff(){}
+
+    public CareerCenterStaff(String userId, String password, String staffDepartment) {
+        super(userId, password, null);
+        this.staffDepartment = staffDepartment;
+    }
+    // Getters
+
+    public String getStaffDepartment() {
+        return staffDepartment;
+    }
+
+    // Setters
+
+    public void setStaffDepartment(String staffDepartment) {
+        this.staffDepartment = staffDepartment;
+    }
 }
