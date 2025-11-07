7e69977830bb76119b6728265563c96699031eae
Darren <darrenweyyh@gmail.com>
2025-11-07 15:45:59 +0800

    feat/student class

--- src/entity/Student.java
+++ src/entity/Student.java
@@ -1,6 +1,19 @@
 package src.entity;
 
+import java.util.ArrayList;
+
 public class Student extends User {
-    public Student() {
+
+    private int YearOfStudy;
+    private String Major;
+    private int applicationCount;
+    private ArrayList<Internship> InternshipApplied;
+
+    public Student(int YearOfStudy, String Major) {
+        this.YearOfStudy = YearOfStudy;
+        this.Major = Major;
+        this.applicationCount = 0;
+        this.InternshipApplied = new ArrayList<>();
     }
+
 }
