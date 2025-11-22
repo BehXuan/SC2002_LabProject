5bff444bcb6045206ac3f6225fd7cc43e26a60d9
Beh Wei Xuan <behweixuan@gmail.com>
2025-11-22 17:06:34 +0800

    Created javadocs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"src"},{"l":"src.controller"},{"l":"src.entity"},{"l":"src.enums"},{"l":"src.interfaces"},{"l":"src.report"},{"l":"src.view"}];updateSearchResults();
